0d88fbfc64a78c5172f8767e0e6ac4a684b0c2e8
Carles Sanchez Romero <carles.sanchez@atmira.com>
2018-05-11 22:21:17 +0200

    Initial commit

---
+++ WcfCalculadora/IService1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfCalculadora
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        int Suma(int num1,int num2);
+        [OperationContract]
+        int Resta(int num1,int num2);
+        [OperationContract]
+        int Multiplicacion(int num1,int num2);
+        [OperationContract]
+        int Division(int num1,int num2);
+
+    }
+}
---
+++ WcfCalculadora/Service1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfCalculadora
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public int Division(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public int Multiplicacion(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Resta(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Suma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
